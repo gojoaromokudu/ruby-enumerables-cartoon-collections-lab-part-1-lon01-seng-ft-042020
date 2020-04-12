4a9f3f23a337d19390639bb2c4dff9b8512fa20b
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-04-12 16:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_index do |index, element|
+  array.each_with_index do |index, element|
    puts "#{index + 1}. #{element}"
    binding.pry
   end
